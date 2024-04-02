daa41dd734f80eb7f89e66abfc453351dfc6d8c2
Fabrice <f.nkurunziz@alustudent.com>
2024-04-02 18:03:36 +0200

    implemented functionalities for sending ajax request to the server
    before I was using json-server to mimic the behaviour of server, but now I have created the actual server and I can send the request to that server

--- Frontend/create-todo-scripts.js
+++ Frontend/create-todo-scripts.js
@@ -1,9 +1,9 @@
-const jsonFile = require("jsonfile");
-
-$("document").ready(() => {
+$(document).ready(() => {
   console.log("hello from the create-todo page");
   $("#myForm").on("submit", function (event) {
+    // Prevent the default form submission behavior
     event.preventDefault();
+
     const formData = new FormData(this);
     let formValues = {};
 
@@ -12,25 +12,15 @@ $("document").ready(() => {
       formValues[key] = value;
     });
 
-    jsonfile.readFile("data.json", (err, data) => {
-      if (err) {
-        console.error("Error reading file:", err);
-        return;
-      }
-
-      // Append new data to the existing JSON object
-      data.push(newData);
-
-      // Write the updated JSON data back to the file
-      jsonfile.writeFile("data.json", data, { spaces: 2 }, (err) => {
-        if (err) {
-          console.error("Error writing file:", err);
-        } else {
-          console.log("Data appended to file successfully.");
-        }
+    // Make a POST request to the server
+    $.post("http://localhost:5000/todo", formValues)
+      .done(function (response) {
+        // Handle successful response
+        console.log("Response from server:", response);
+      })
+      .fail(function (xhr, status, error) {
+        // Handle errors
+        console.error("Error:", error);
       });
-    });
-
-    console.log(formValues);
   });
 });
